aeffcb2ac09805d338300cf316d936048b43bbcd
AlistairStrange <marekjankovic0@gmail.com>
2021-08-21 12:06:33 +0200

    Error message adjusted

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container mx-auto my-5">
-    <p class="font-light text-gray-500">Ticket with specified ID was not found</p>
+    <p class="font-light text-gray-500">Ticket with your ID was not found</p>
   </div>
 </template>
