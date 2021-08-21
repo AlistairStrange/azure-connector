04b6eaaabfb94c93bd3b8a3513dc2d83f95accbd
AlistairStrange <marekjankovic0@gmail.com>
2021-08-21 14:33:24 +0200

    Format / indentation change

--- src/components/AddComment.vue
+++ src/components/AddComment.vue
@@ -1,35 +1,54 @@
 <template>
-  <div class="w-full container my-2 px-4 bg-gray-200 py-2 rounded-md float-right">
-    <p class="text-gray-400 hover:text-gray-500 float-right cursor-pointer" @click="hideComments">Close x</p>
+  <div
+    class="w-full container my-2 px-4 bg-gray-200 py-2 rounded-md float-right"
+  >
+    <p
+      class="text-gray-400 hover:text-gray-500 float-right cursor-pointer"
+      @click="hideComments"
+    >
+      Close x
+    </p>
     <form @submit.prevent="submitComment">
       <!--Email  -->
-        <label for="email" class="mb-2 mx-2 font-medium text-gray-500">From</label>
-        <input
-          class="w-8/12 rounded-md bg-gray-100 block px-2 py-2 mx-2 mb-2 text-gray-400"
-          placeholder="Enter your email address"
-          name="email"
-          type="email"
-          v-model="comment.email"
-        />
+      <label for="email" class="mb-2 mx-2 font-medium text-gray-500"
+        >From</label
+      >
+      <input
+        class="w-8/12 rounded-md bg-gray-100 block px-2 py-2 mx-2 mb-2 text-gray-400"
+        placeholder="Enter your email address"
+        name="email"
+        type="email"
+        v-model="comment.email"
+      />
 
-        <!-- Text -->
-        <label for="comment" class="mb-2 mx-2 font-medium text-gray-500">Text</label>
-        <textarea
-          class="w-11/12 rounded-md bg-gray-100 block px-2 mx-2 text-gray-400"
-          name="comment"
-          v-model="comment.text"
-          placeholder="Enter your comment"
-          rows="5"
-        />
+      <!-- Text -->
+      <label for="comment" class="mb-2 mx-2 font-medium text-gray-500"
+        >Text</label
+      >
+      <textarea
+        class="w-11/12 rounded-md bg-gray-100 block px-2 mx-2 text-gray-400"
+        name="comment"
+        v-model="comment.text"
+        placeholder="Enter your comment"
+        rows="5"
+      />
 
-        <button class="text-white font-bold py-2 px-4 mx-2 my-4" v-if="!isLoading" type="submit">
-          Submit
-        </button>
+      <button
+        class="text-white font-bold py-2 px-4 mx-2 my-4"
+        v-if="!isLoading"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
 
     <loading-wheel v-if="isLoading"></loading-wheel>
-    <p v-if="success" class="text-gray-500 font-light mx-2">Comment was successfully posted</p>
-    <p v-if="failure" class="text-gray-500 font-light mx-2">Ooops, something went wrong</p>
+    <p v-if="success" class="text-gray-500 font-light mx-2">
+      Comment was successfully posted
+    </p>
+    <p v-if="failure" class="text-gray-500 font-light mx-2">
+      Ooops, something went wrong
+    </p>
   </div>
 </template>
 
@@ -43,11 +62,12 @@ export default {
   props: {
     id: Number,
   },
-  emits: ['hide-comments'],
-  setup(props, {emit}) {
+  emits: ["hide-comments"],
+  setup(props, { emit }) {
     var comment = reactive({
       email: null,
       text: null,
+      files: null,
       id: props.id,
     });
 
@@ -57,7 +77,7 @@ export default {
     const apiUrl =
       "https://prod-240.westeurope.logic.azure.com:443/workflows/1f4be62774a24ff49d688af3dc78dcaf/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=AFvlS67qtIoUDeTCJBs94t1ywQDNojG7BoKPfiCm-dQ";
 
-    function hideComments(){
+    function hideComments() {
       emit("hide-comments");
     }
 
@@ -92,6 +112,7 @@ export default {
       comment,
       submitComment,
       hideComments,
+      onFileChange,
       isLoading,
       success,
       failure,
