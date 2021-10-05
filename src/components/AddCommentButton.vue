19262c4928a706db61a34727cbfacab2ea32594a
AlistairStrange <marekjankovic0@gmail.com>
2021-10-05 21:27:36 +0200

    HeroIcon added

--- src/components/AddCommentButton.vue
+++ src/components/AddCommentButton.vue
@@ -1,5 +1,15 @@
 <template>
-  <p class="font-medium text-gray-400 hover:text-gray-500 float-right cursor-pointer my-3">
-    + Comments
-  </p>
+  <div class="font-medium text-gray-400 hover:text-gray-500 float-right cursor-pointer my-3">
+    <ChatAlt2Icon class="h-5 w-5 inline"></ChatAlt2Icon>
+    Discussion
+  </div>
 </template>
+
+<script>
+import { ChatAlt2Icon } from "@heroicons/vue/solid";
+
+export default {
+  components: { ChatAlt2Icon },
+  setup() {},
+};
+</script>
